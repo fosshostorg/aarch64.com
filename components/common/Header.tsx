3f461bc1e155ef19e6d0080286fe4a27a09cc39a
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 14:13:05 +0530

    Add Header with NavBar

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,2 +1,13 @@
-const Header = () => <header></header>;
+import styles from "@/styles/Header.module.scss";
+import NavBar from "@/components/common/NavBar";
+
+const Header = () => (
+  <header className={styles.header}>
+    <div className={styles.logo}>
+      <img src="/logo.png" alt="ARM-64.com Logo" />
+    </div>
+    <NavBar />
+  </header>
+);
+
 export default Header;
