02456907ce394ca158f68ec652135eb7a46ea267
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 19:34:55 +0530

    Add Footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,2 +1,25 @@
-const Footer = () => <footer></footer>;
+import styles from "@/styles/Footer.module.scss";
+import Link from "next/link";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.left}>
+      <h2>Made by Fosshost.</h2>
+      <p>
+        We are not affiliated with Arm Holdings or any of it’s subsidiaries. We
+        are a Fosshost project.
+      </p>
+      <img src="/logo.png" alt="ARM-64.com Logo" />
+    </div>
+    <div className={styles.right}>
+      <div className={styles.links}>
+        <h3>Links</h3>
+        <Link href="/">Contact Us</Link>
+        <Link href="/">Our Policy</Link>
+      </div>
+      <div className={styles.social}></div>
+    </div>
+  </footer>
+);
+
 export default Footer;
