a96e73235b5a597ed55ef9830aefe39babe8b511
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 14:22:24 +0530

    Move default HEAD tags from index to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Header from "@/components/common/Header";
 import Hero from "@/components/HomePage/Hero";
 import Sponsors from "@/components/HomePage/Sponsors";
@@ -7,14 +6,6 @@ import Footer from "@/components/common/Footer";
 
 const Home = () => (
   <>
-    <Head>
-      <title>ARM-64.com</title>
-      <meta
-        name="description"
-        content="Blog ran by experts about the challenges of planning, coordinating and managing 50u of ARM hardware in multiple, global locations."
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
     <Header />
     <Hero />
     <Sponsors />
