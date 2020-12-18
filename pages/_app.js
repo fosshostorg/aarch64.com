c9a08b4420f0a659a7c529dd2e1d2fb9a41c10fb
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 14:21:55 +0530

    Add default HEAD tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ARM-64.com</title>
+        <meta
+          name="description"
+          content="Blog ran by experts about the challenges of planning, coordinating and managing 50u of ARM hardware in multiple, global locations."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
