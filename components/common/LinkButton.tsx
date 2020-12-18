a92834dca35853fbee857c76b758f0ed36089d2b
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 17:56:54 +0530

    Add Button component

--- components/common/LinkButton.tsx
+++ components/common/LinkButton.tsx
@@ -1,2 +1,31 @@
-const LinkButton = () => <a href=""></a>;
+import Link from "next/link";
+import styles from "@/styles/Button.module.scss";
+
+type LinkButtonProps = {
+  children: any;
+  href: string;
+  external?: boolean;
+};
+
+const LinkButton = ({ children, href, external }: LinkButtonProps) => {
+  if (external) {
+    return (
+      <a
+        className={styles.button}
+        href={href}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <Link href={href}>
+        <a className={styles.button}>{children}</a>
+      </Link>
+    );
+  }
+};
+
 export default LinkButton;
