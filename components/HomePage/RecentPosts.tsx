b8a4bc8ec508b38afa23c87d918520b16762735f
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 21:45:02 +0530

    Add "Recent Posts" section

--- components/HomePage/RecentPosts.tsx
+++ components/HomePage/RecentPosts.tsx
@@ -1,3 +1,49 @@
-const RecentPosts = () => <div></div>;
+import styles from "@/styles/HomePage/RecentPosts.module.scss";
+
+const RecentPosts = () => (
+  <div className={styles.recentPosts}>
+    <h3>RECENT POSTS</h3>
+    <div className={styles.posts}>
+      <div className={styles.post}>
+        <h1>
+          This is a title for a blog post that spans multiple lines, who knows
+          how many.
+        </h1>
+        <div className={styles.meta}>
+          <div className={styles.avatar}>
+            <img src="/avatars/avatar1.png" alt="John Doe" />
+          </div>
+          <div className={styles.author}>John Doe</div>
+          <div className={styles.separator}>·</div>
+          <div className={styles.date}>Dec 17, 2020</div>
+        </div>
+      </div>
+      <div className={styles.others}>
+        <div className={styles.post}>
+          <h1>This title is a little bit smaller.</h1>
+          <div className={styles.meta}>
+            <div className={styles.avatar}>
+              <img src="/avatars/avatar2.png" alt="John Doe" />
+            </div>
+            <div className={styles.author}>Jane Doe</div>
+            <div className={styles.separator}>·</div>
+            <div className={styles.date}>Dec 17, 2020</div>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h1>Very small title.</h1>
+          <div className={styles.meta}>
+            <div className={styles.avatar}>
+              <img src="/avatars/avatar3.png" alt="John Doe" />
+            </div>
+            <div className={styles.author}>Dane Joe</div>
+            <div className={styles.separator}>·</div>
+            <div className={styles.date}>Dec 17, 2020</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default RecentPosts;
