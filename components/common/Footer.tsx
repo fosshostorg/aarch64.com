536da42d8da915e43024766b8c30d7ba38a10446
Aman Harwara <amanharwara@protonmail.com>
2020-12-18 20:49:44 +0530

    Modify footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,11 +5,6 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.left}>
       <h2>Made by Fosshost.</h2>
-      <p>
-        We are not affiliated with Arm Holdings or any of it’s subsidiaries. We
-        are a Fosshost project.
-      </p>
-      <img src="/logo.png" alt="Arm-64.com Logo" />
     </div>
     <div className={styles.right}>
       <div className={styles.links}>
