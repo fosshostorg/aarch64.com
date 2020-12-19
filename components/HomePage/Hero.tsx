8a7c96c20db7c6ef720fb5c6b92ba21fa0fa11da
Aman Harwara <amanharwara@protonmail.com>
2020-12-19 23:26:21 +0530

    Fix Hero section copy

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => (
     <div className={styles.left}>
       <h1>Our journey of ARMing the open source community</h1>
       <p>
-        Daily blog run by experts about the challenges of planning, coordinating
-        and managing 50u of Arm hardware in multiple, global locations.
+        Daily blog, run by experts, on the challenges of planning, coordinating
+        and managing 50 units of ARM hardware in multiple, global locations.
       </p>
       <LinkButton href="/blog">Read The Blog</LinkButton>
     </div>
