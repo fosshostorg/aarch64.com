5bb0b982f6f89ee8def285f03f701b815b562936
Aman Harwara <amanharwara@protonmail.com>
2020-12-19 23:26:10 +0530

    Fix sponsor descriptios

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -5,7 +5,7 @@ const Sponsors: Sponsor[] = [
     name: "Ampere Computing",
     link: "https://amperecomputing.com/",
     description:
-      "Ampere’s vision is to set the new standard for cloud and edge servers and reset expectations for high performance and a better total cost of ownership. Their eMAG system is designed with cloud workloads in mind, with 32 Armv8 cores at 3.3 GHz in a single socket configuration.",
+      "Ampere’s vision is to set the new standard for cloud and edge servers, reset expectations for high performance, and a better total cost of ownership. Their eMAG system is designed with cloud workloads in mind, with 32 Armv8 cores at 3.3GHz in a single socket configuration.",
   },
   {
     id: "custodian",
@@ -13,7 +13,7 @@ const Sponsors: Sponsor[] = [
     name: "CustodianDC",
     link: "https://www.custodiandc.com/",
     description:
-      "Our Kent Data Centre is ideally situated 30 minutes south east of London, outside of high-risk zones but still within easy reach of London and Kent's surrounding areas. Offering almost unlimited power and space, renowned connectivity, high level security and ISO 27001 certification there really is no better place to house your infrastructure.",
+      "Our Kent Data Centre is ideally situated 30 minutes south east of London, outside of high-risk zones but still within easy reach of London and Kent's surrounding areas. Offering almost unlimited power and space, renowned connectivity, high level security and ISO 27001 certification, there is no better place to house your infrastructure.",
   },
   {
     id: "hive",
@@ -21,7 +21,7 @@ const Sponsors: Sponsor[] = [
     name: "HiveDC",
     link: "https://hivedatacenter.com/",
     description:
-      "Hive is a Colocation Data Center provider based in Montreal. With customers all around the world, Hive has become a leading choice for per U and cabinet server colocation in Canada.  Led by it’s co-founders, Karl Morin, Etienne Champagne and Emile Ménard, the fast-growing company aims to create a stunning, high-performance environnement for the IT needs of small and large companies.",
+      "Hive is a Colocation Data Center provider based in Montreal. With customers all around the world, Hive has become a leading choice for per unit and cabinet server colocation in Canada. Led by it’s co-founders, Karl Morin, Etienne Champagne and Emile Ménard, this fast-growing company aims to create a stunning, high-performance environement for the IT needs of small and large companies.",
   },
   {
     id: "webnx",
@@ -29,7 +29,7 @@ const Sponsors: Sponsor[] = [
     name: "WebNX",
     link: "http://www.webnx.com/",
     description:
-      "At WebNX we pride ourselves on offering Enterprise Internet Infrastructure Solutions at affordable prices. We combine top quality components from the top manufacturers with our 100% self-operated data centers and our Route Optimized Networks to bring you IT values unlike anyone else. We offer Dedicated Servers and Colocation Options. With over 15 years industry experience, your enterprise is in good hands with WebNX Internet Services.",
+      "At WebNX, we pride ourselves on offering Enterprise Internet Infrastructure Solutions at affordable prices. We combine top quality components from the top manufacturers with our 100% self-operated data centers, and our Route Optimized Networks, to bring you IT values unlike anyone else. We offer Dedicated Servers and Colocation Options. With over 15 years industry experience, your enterprise is in good hands with WebNX Internet Services.",
   },
   {
     id: "inferno",
@@ -37,7 +37,7 @@ const Sponsors: Sponsor[] = [
     name: "Inferno Communications",
     link: "https://infernocomms.com/",
     description:
-      "Inferno Communications is a managed service IT provider that operates predominantly within the United Kingdom providing dedicated and colocation services from Digital Realty Facilities including Interxion London.",
+      "Inferno Communications is a managed service IT provider that operates predominantly within the United Kingdom, providing dedicated and colocation services from Digital Realty Facilities, including Interxion London.",
   },
   {
     id: "equinix",
@@ -45,7 +45,7 @@ const Sponsors: Sponsor[] = [
     name: "Equinix Metal",
     link: "http://metal.equinix.com/",
     description:
-      "As part of Equinix — the world’s #DigitalInfrastructure company — Equinix Metal provides automated & interconnected bare metal infrastructure. Formerly Packet, now Equinix Metal™.  Ed Vielmetti also runs the Worksonarm.com Project who we will be working closely with.",
+      "As part of Equinix — the world’s #DigitalInfrastructure company — Equinix Metal provides automated and interconnected bare-metal infrastructure. Formerly Packet, now Equinix Metal™. Ed Vielmetti also runs the Worksonarm.com Project, who we will be working closely with.",
   },
   {
     id: "packetframe",
@@ -53,7 +53,7 @@ const Sponsors: Sponsor[] = [
     name: "PacketFrame",
     link: "http://www.packetframe.com/",
     description:
-      "PacketFrame is an open-source project that aims to deliver CDN, DNS, caching and Anycast services for projects.  The project works closely with Fosshost and has shared engineering DevOps and SysOp capabilities.  Overall, the Packetframe and Fosshost engineering teams will be crucial to the success of the project.",
+      "PacketFrame is an open-source project that aims to deliver CDN, DNS, caching and Anycast services for projects. PacketFrame works closely with Fosshost and has shared engineering DevOps and SysOp capabilities. Overall, the Packetframe and Fosshost engineering teams will be crucial to the success of the project.",
   },
 ];
 
