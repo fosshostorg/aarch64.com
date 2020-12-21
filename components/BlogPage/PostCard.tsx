235d283329fef8b502cc008a487b7e5f4bcbfb57
Aman Harwara <amanharwara@protonmail.com>
2020-12-21 18:20:13 +0530

    Separate post cards from the blog index

---
+++ components/BlogPage/PostCard.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "@/styles/Blog/BlogPage.module.scss";
+
+const PostCard = ({ post }) => (
+  <Link href={`/post/${post.slug}`}>
+    <a href={`/post/${post.slug}`} className={styles.post}>
+      <div className={styles.thumbnail}>
+        <div className={styles.overlay} />
+        <img
+          src={`/thumbnails/${post.thumbnail}`}
+          alt={`Thumbnail for ${post.title}`}
+        />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.date}>{post.date}</div>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.meta}>
+          <div className={styles.avatar}>
+            <img
+              src={`/avatars/${post.author
+                .toLowerCase()
+                .replace(/\W/g, "-")}.png`}
+              alt={`Image of ${post.author}`}
+            />
+          </div>
+          <div className={styles.author}>{post.author}</div>
+        </div>
+      </div>
+    </a>
+  </Link>
+);
+
+export default PostCard;
