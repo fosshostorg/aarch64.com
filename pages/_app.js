df7df4a47c18682b5c88fcbe38847fd174310a5b
Aman Harwara <amanharwara@protonmail.com>
2020-12-22 17:28:24 +0530

    Use new favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
           content="Daily blog run by experts about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
