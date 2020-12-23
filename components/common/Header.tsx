3222bd013e73820dff0e4e01153219823f6a34e2
Aman Harwara <amanharwara@protonmail.com>
2020-12-23 14:53:29 +0530

    Remove link from header logo

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,11 +3,11 @@ import NavBar from "@/components/common/NavBar";
 
 const Header = () => (
   <header className={styles.header}>
-    <a href="/" className={styles.logo}>
+    <div className={styles.logo}>
       <object data="/logo.svg" type="image/svg+xml">
         <img src="/logo.png" alt="Arm-64.com Logo" />
       </object>
-    </a>
+    </div>
     <NavBar />
   </header>
 );
