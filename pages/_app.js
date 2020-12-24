31de4b7187aef57afdcf14dca6aa1e37b658e32c
Aman Harwara <amanharwara@protonmail.com>
2020-12-24 11:09:24 +0530

    Fix some meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <html lang="en" />
         <title>Arm-64.com</title>
         <meta
           name="description"
           content="Daily blog run by experts about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations."
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.png" />
         <script
           async
---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
