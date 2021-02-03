7676095c3c01ea273ebac1d93986fbd8e2b74667
fosshostorg <64111949+fosshostorg@users.noreply.github.com>
2021-02-03 21:51:29 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Arm-64.com</title>
+        <title>AArch64.com</title>
         <meta
           name="description"
-          content="Daily blog run by experts about the challenges of planning, coordinating and managing 50u of Arm hardware in multiple, global locations."
+          content="Daily blog, run by experts, on the challenges of planning, coordinating and managing 50 units of Arm-based hardware in multiple, global locations."
         />
         <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script
           async
           defer
-          data-domain="arm-64.com"
+          data-domain="aarch64.com"
           src="https://plausible.io/js/plausible.js"
         ></script>
       </Head>
