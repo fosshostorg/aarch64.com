e1f0ba65b65d1b7cfd1ce6ddd25f2b4867f7202a
Bruno Miguel <brunoalexandremiguel@gmail.com>
2021-04-03 01:05:10 +0100

    fixed in the homepage hero section

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -4,7 +4,7 @@ import LinkButton from "../common/LinkButton";
 const Hero = () => (
   <main className={styles.hero}>
     <div className={styles.left}>
-      <h1>Our journey of providing Arm hosting to the open source community</h1>
+      <h1>Our journey of providing Arm-based hosting to the open-source community</h1>
       <p>
         Daily blog, run by experts, on the challenges of planning, coordinating
         and managing 50 units of Arm-based hardware in multiple, global
