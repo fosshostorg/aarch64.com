23c288e973b1b119d04b885af60e7a34a743df42
Chris Mills <7100370+chriscoffee@users.noreply.github.com>
2021-07-02 11:59:22 +0100

    Add CSP and other security headers
    Signed-off-by: Chris Mills <7100370+chriscoffee@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,39 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([[optimizedImages, {}]], {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; base-uri 'self'; script-src 'self' 'unsafe-eval' plausible.io *.cloudfront.net; style-src 'self' 'unsafe-inline'; img-src 'self' *.cloudfront.net data:",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },          
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },          
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self "https://aarch64.com"), microphone=()',
+          }
+        ]
+      },
+    ]
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
