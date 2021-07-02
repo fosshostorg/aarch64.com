78260e114b0047879512f09ac6984571ce03ab0b
Chris Mills <7100370+chriscoffee@users.noreply.github.com>
2021-07-02 10:03:46 +0100

    Correct comments section
    Signed-off-by: Chris Mills <7100370+chriscoffee@users.noreply.github.com>

--- components/BlogPage/CommentSection.tsx
+++ components/BlogPage/CommentSection.tsx
@@ -5,7 +5,7 @@ const CommentSection = () => {
   return (
     <section>
       <ReactUtterances
-        repo="fosshostorg/arm-64.com"
+        repo="fosshostorg/aarch64.com"
         type="pathname"
         label="blog-comment"
         theme="github-select"
