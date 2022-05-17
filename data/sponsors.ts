9d3164317fd606ab46bb1681d06027cfbce97722
Bruno Miguel <brunoalexandremiguel@gmail.com>
2022-05-17 18:56:10 +0100

    added netwise to sponsors

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -10,6 +10,14 @@ const Sponsors: Sponsor[] = [
       "Ampere’s vision is to set the new standard for cloud and edge servers, reset expectations for high performance, and a better total cost of ownership. Their eMAG system is designed with cloud workloads in mind, with 32 Armv8 cores at 3.3GHz in a single socket configuration.",
   },
   {
+    id: "ans",
+    img: "ans-logo.svg",
+    name: "ANS Group",
+    link: "https://www.ans.co.uk/",
+    description:
+    "Delivering and executing digital transformation is extremely complex. Even defining digital transformation can be difficult when it means different things to every organisation. And even when you’ve mapped our your digital journey, you would typically have to engage with multiple partners to acquire services, adding yet more complexity, time and cost. At ANS, we’ve removed all of this complexity by enabling you to unlock the power of 6 technical capabilities under one programme of work without the stress of having to rely on multiple vendors.",
+  },
+  {
     id: "custodian",
     img: "custodian.svg",
     name: "CustodianDC",
@@ -26,20 +34,20 @@ const Sponsors: Sponsor[] = [
       "Hive is a Colocation Data Center provider based in Montreal. With customers all around the world, Hive has become a leading choice for per unit and cabinet server colocation in Canada. Led by it’s co-founders, Karl Morin, Etienne Champagne and Emile Ménard, this fast-growing company aims to create a stunning, high-performance environement for the IT needs of small and large companies.",
   },
   {
+    id: "netwise",
+    img: "netwise-logo.png",
+    name: "Netwise",
+    link: "https://www.netwise.co.uk/",
+    description:
+    "Founded in 2009, with a genesis story stretching back further still, Netwise started life as a passion project by two school friends with a love for technology. Fast forward nearly a decade, and we are now one of the UK's leading colocation service providers, with private facilities designed and built entirely in-house, delivering end-user content on a truly global scale.",
+  },
+  {
     id: "webnx",
     img: "webnx.png",
     name: "WebNX",
     link: "http://www.webnx.com/",
     description:
       "At WebNX, we pride ourselves on offering Enterprise Internet Infrastructure Solutions at affordable prices. We combine top quality components from the top manufacturers with our 100% self-operated data centers, and our Route Optimized Networks, to bring you IT values unlike anyone else. We offer Dedicated Servers and Colocation Options. With over 15 years industry experience, your enterprise is in good hands with WebNX Internet Services.",
-  },
-  {
-  id: "ans",
-  img: "ans-logo.svg",
-  name: "ANS Group",
-  link: "https://www.ans.co.uk/",
-  description:
-  "Delivering and executing digital transformation is extremely complex. Even defining digital transformation can be difficult when it means different things to every organisation. And even when you’ve mapped our your digital journey, you would typically have to engage with multiple partners to acquire services, adding yet more complexity, time and cost. At ANS, we’ve removed all of this complexity by enabling you to unlock the power of 6 technical capabilities under one programme of work without the stress of having to rely on multiple vendors.",
   },
 ];
 
